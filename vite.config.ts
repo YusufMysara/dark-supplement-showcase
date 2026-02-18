b5f82d99d27d1b8b7d3f4bb69946fe2661334022
Youssef <yusuf.mysaraa2@gmail.com>
2026-02-18 19:14:35 +0200

    .

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,12 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
+export default defineConfig({
+  base: "./",
+  build: {
+    outDir: "dist",
+    emptyOutDir: true,
+  },
   server: {
     host: "::",
     port: 8080,
@@ -17,4 +22,4 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-}));
+});
